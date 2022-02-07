8b13ca65117b9572a43b617e3be3f0f5fb5eeff8
artbikin <corinthianorder.arch@gmail.com>
2022-02-06 20:35:06 -0800

    Initial commit1

---
+++ src/test/java/AzzaSecrectLanguage.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+
+public class AzzaSecrectLanguage {
+
+    @Test
+
+    public void test1(){
+        System.out.println("dfsdfs");
+    }
+
+    @Test
+
+    public void test2(){
+        System.out.println("some text");
+    }
+    @Test
+
+    public void test3(){
+        System.out.println("close browser");
+    }
+
+}
